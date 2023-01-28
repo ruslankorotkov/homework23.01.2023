9c20d5950942ba3ff6c5a6cd4777ecf8f57ebb47
ruslankorotkov <richard2008@rus>
2023-01-28 10:28:32 +0300

    Хеш и хеш-функции, множества  23/01/2023  ( доработка )

--- src/Main.java
+++ src/Main.java
@@ -111,7 +111,7 @@ public class Main {
 //        System.out.println(" множество водителей ");
 //        System.out.println(Arrays.toString(driversSetList.toArray()));
         System.out.println(" Создал 2 разных объекта, но с одинаковыми значениями полей: " + ivan + " хешкод " +
-                ivan.hashCode() + " yvan " + yvan + "  хешкод " + yvan.hashCode());
+                ivan.hashCode() + "" + yvan + "  хешкод " + yvan.hashCode());
         Iterator<Driver> iteratorDriver = driversSetList.iterator();
         while (iteratorDriver.hasNext()) {
             System.out.println(iteratorDriver.next());
