6c2ebd4c2025f0d07c2c631792e6ede9c70b69d5
ruslankorotkov <richard2008@rus>
2023-01-28 10:20:28 +0300

    Хеш и хеш-функции, множества  23/01/2023  ( доработка )

--- src/Main.java
+++ src/Main.java
@@ -115,6 +115,7 @@ public class Main {
         Iterator<Driver> iteratorDriver = driversSetList.iterator();
         while (iteratorDriver.hasNext()) {
             System.out.println(iteratorDriver.next());
+
         }
     }
 //    public static void getMechList(Mechanic... mechanics) {
